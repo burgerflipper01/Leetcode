e3ed0380e1a673e555401f29c761ecb3f18648fe
AndersMikkelsen <ande077h@gmail.com>
2023-05-24 10:16:32 +0200

    Leetcode solutions

---
+++ src/romanToInteger/Solution.java
@@ -0,0 +1,44 @@
+package romanToInteger;
+
+public class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char symbol = s.charAt(i);
+            int value = getValue(symbol);
+
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+
+            prevValue = value;
+        }
+
+        return result;
+    }
+
+    private int getValue(char symbol) {
+        switch (symbol) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                throw new IllegalArgumentException("Invalid symbol: " + symbol);
+        }
+    }
+}
